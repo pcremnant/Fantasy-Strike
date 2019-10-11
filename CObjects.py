fd70b5ada8d63f71d6190570f1aa81767cf9f50c
ParkChan <pcremnant@naver.com>
2019-10-11 18:17:21 +0900

    add buildPointer

--- CObjects.py
+++ CObjects.py
@@ -41,7 +41,7 @@ class Obj_Build(Obj):
     global cBuildMap
 
     def __init__(self, x, y, sizeX, sizeY, imgPath):
-        super().__init__(x, y, sizeX, sizeY ,imgPath)
+        super().__init__(x, y, sizeX, sizeY, imgPath)
         # 필요한 자원 리소스들 추가
         self.nResource = 0  # 임시 자원 변수
 
@@ -50,4 +50,13 @@ class Obj_Build(Obj):
         # self.posObject.SetPosition(posMouse.posX - (posMouse.posX % 16), posMouse.posY - (posMouse.posY % 16))
         # 16픽셀 단위로 이동 가능하게 설정
 
+
+class Obj_BuildPointer(Obj):
+    global cBuildMap
+
+    def __int__(self, x, y, sizeX, sizeY, imgPath):
+        super().__init__(x, y, sizeX, sizeY, imgPath)
+
+    def GetMousePosition(self, x, y):
+        self.SetPosition(cBuildMap.GetPositionX(x), cBuildMap.GetPositionY(y))
 # 마우스 포인터를 받아서 건물을 짓는다
--- framework.py
+++ framework.py
@@ -5,14 +5,19 @@ import CStruct as STRUCT
 
 open_canvas(DEFINE.WINDOW_WIDTH, DEFINE.WINDOW_HEIGHT)
 
-nMouseX = 300
-nMouseY = 400
+nMouseX = 0
+nMouseY = 0
+nClickedMouseX = 300
+nClickedMouseY = 400
 
 
 def handle_events():
     global running
+    global nClickedMouseX
+    global nClickedMouseY
     global nMouseX
     global nMouseY
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -21,13 +26,20 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_MOUSEBUTTONDOWN:
+            nClickedMouseX = event.x
+            nClickedMouseY = DEFINE.WINDOW_HEIGHT - event.y
+        elif event.type == SDL_MOUSEMOTION:
             nMouseX = event.x
             nMouseY = DEFINE.WINDOW_HEIGHT - event.y
+            buildPointer.GetMousePosition(nMouseX, nMouseY)
     pass
 
 
-build = OBJECT.Obj_Build(400, 300, 2, 2, "tmpTree.png")
+build = OBJECT.Obj_Build(400, 300, 2, 2, "tree_A.png")
 build.SetObjectImage(1, 64, 64)
+
+buildPointer = OBJECT.Obj_BuildPointer(nMouseX, nMouseY, 2, 2, "tile_g.png")
+buildPointer.SetObjectImage(1, 64, 64)
 
 buildMap = STRUCT.SBuild_Map(OBJECT.cBuildMap.GetStartX(), OBJECT.cBuildMap.GetStartY(),
                              OBJECT.cBuildMap.GetTileWidth(), OBJECT.cBuildMap.GetTileHeight())
@@ -37,7 +49,8 @@ running = True
 while running:
     clear_canvas()
 
-    build.BuildObject(nMouseX, nMouseY)
+    buildPointer.DrawObject()
+    build.BuildObject(nClickedMouseX, nClickedMouseY)
     build.DrawObject()
     buildMap.tmpDrawTable()
 
